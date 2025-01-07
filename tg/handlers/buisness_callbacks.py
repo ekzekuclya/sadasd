43bfecb8323d582fcf6b30d2fad458def742b199
ekzekuclya <ekzekuciya07@gmail.com>
2025-01-07 19:11:21 +0500

    fdssf

--- tg/handlers/buisness_callbacks.py
+++ tg/handlers/buisness_callbacks.py
@@ -73,7 +73,7 @@ async def check_ltc(msg: Message):
             user.last_message_time = timezone.now()
             user.save()
         if user.is_admin:
-            withdraw = await sync_to_async(Withdraw.objects.filter)(chat_id=msg.chat.id)
+            withdraw = await sync_to_async(Withdraw.objects.filter)(chat_id=msg.chat.id, active=True)
             withdraw = withdraw.first()
             await msg.answer(f"{withdraw.symbol} `{withdraw.amount}`\n\n`{msg.text}`", parse_mode="Markdown")
     except Exception as e:
