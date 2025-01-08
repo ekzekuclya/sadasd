455d2118fa833022204a88bd2b2be5641b640023
ekzekuclya <ekzekuciya07@gmail.com>
2025-01-09 01:29:24 +0500

    fdsfgsg

--- tg/handlers/photo_utils.py
+++ tg/handlers/photo_utils.py
@@ -17,7 +17,9 @@ async def draw_image(data):
     draw = ImageDraw.Draw(image)
 
     # font_path = current_dir / "Roboto/Roboto-Medium.ttf"
-    font = ImageFont.truetype("tg/handlers/Roboto/Roboto-Medium.ttf", size=40)
+    # font = ImageFont.truetype("tg/handlers/Roboto/Roboto-Medium.ttf", size=40)
+    font = ImageFont.load_default()
+
     color = (255, 255, 255)
 
     # Текст, который мы хотим нарисовать
