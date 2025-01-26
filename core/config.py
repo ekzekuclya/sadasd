ea8e522cca67c4f92644f7ef61704173bc96424c
ekzekuclya <147051200+ekzekuclya@users.noreply.github.com>
2025-01-26 19:01:58 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,4 +1,4 @@
 from aiogram import Router, Bot, F
 
 bot_main = Bot(token="7946803014:AAHy2p3iQ22N1llh7mE4CIkSzAMshvYiYUM")
-bot_oper = Bot(token="7598659064:AAF8jqTNE0HP4YTMALuNj1_ng_-rbw7TkDg")
+# bot_oper = Bot(token="7598659064:AAF8jqTNE0HP4YTMALuNj1_ng_-rbw7TkDg")
